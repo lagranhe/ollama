a2f90442c1665078a5e75684612267292cc0394c
Tomasz Hanusiak <T.Hanusiak@pl.ibm.com>
2026-03-06 12:05:19 +0100

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-mkdir ollama
-cd ollama
+#git clone https://github.com/thanusiak/ollama.git
+#cd ollama
+#git fetch origin 
+#git switch upgrade-go-to-1.24.6
+#git pull
+
+mkdir /root/ollama
+cd /root/ollama
 dnf install git zstd docker -y
-git clone https://github.com/thanusiak/ollama.git
-cd ollama
-git fetch origin 
-git switch upgrade-go-to-1.24.6
-git pull
-./scripts/build_linux.sh
+s./scripts/build_linux.sh
